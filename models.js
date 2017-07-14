3377829df450c485d76c9bdac0063c1ff5ae7133
SLandgra <slandgra@ucsd.edu>
2017-07-14 12:35:16 -0700

    Models update

--- models.js
+++ models.js
@@ -1,15 +1,60 @@
 var mongoose = require('mongoose');
+var connect = process.env.MONGODB_URI;
 
+// If you're getting an error here, it's probably because
+// your connect string is not defined or incorrect.
+mongoose.connect(connect);
+
+// Step 1: Write your schemas here!
+// Remember: schemas are like your blueprint, and models
+// are like your building!
 var userSchema = mongoose.Schema({
-  username: String,
+  name: String,
   password: String,
-  phone: String
+  email: String,
+  major: String,
+  school: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'School'
+  }
+  classes: String,
+  sellerrating: Number,
+  notes: Array
 });
 
+var productSchema = mongoose.Schema({
+  name: String,
+  pdf: String,
+  owner: String,
+  price: String,
+  course: String,
+  school: String
+});
 
+var schoolSchema = mongoose.Schema({
+  name: String,
+  courses: Array,
+  majors: Array
+})
 
-User = mongoose.model('User', userSchema);
+var reviewSchema = mongoose.Schema({
+  owner:{
+    type: mongoose.Schema.ObjectId,
+    ref: 'User'
+  },
+  content:String,
+  rating: Number
+});
 
-module.exports = {
-    User:User
-};
+// Step 2: Create all of your models here, as properties.
+var User = mongoose.model('User', userSchema);
+var Product = mongoose.model('Restaurant', productSchema);
+var Review = mongoose.model('Review', reviewSchema);
+var School = mongoose.model('School',schoolSchema)
+// Step 3: Export your models object
+module.exports  = {
+  User: User,
+  Product: Product,
+  Review: Review,
+  School: School
+}
