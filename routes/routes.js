70aa456463b04bf70d1aaa1ee1a680782b31fca0
SLandgra <slandgra@ucsd.edu>
2017-07-14 17:53:51 -0700

    marketplace route

--- routes/routes.js
+++ routes/routes.js
@@ -61,8 +61,7 @@ router.use(function(req, res, next){
 //////////////////////////////// PRIVATE ROUTES ////////////////////////////////
 // Only logged in users can see these routes
 
-router.get('/marketplace', function(req, res, next) {
-  var products = [];
+router.get('/marketplace', function(req, res) {
   Product.find({school: req.user.school, subject:req.user.major})
   .populate('school')
   .populate('owner')
@@ -72,8 +71,34 @@ router.get('/marketplace', function(req, res, next) {
         product:doc
       }))
     })
-
   });
+router.post('/marketplace', function(req,res){
+  var find={
+    subject: req.body.subject
+  };
+  var sort = {}
+  if(req.body.sort = 'Rating'){
+    sort = {
+      owner.sellerrating: 1
+    }
+  }else if(req.body.sort = 'Price Ascending'){
+    sort = {
+      price: -1
+    }
+  }else{
+    sort = {
+      price: 1
+    }
+  }
+
+  if('all'!==req.body.school){
+    find.school=req.body.school;
+  }
+  Product.find(find)
+  .populate('school')
+  .populate('owner')
+  .sort(req.)
+})
 
   ///////////////////////////// END OF PRIVATE ROUTES /////////////////////////////
 
