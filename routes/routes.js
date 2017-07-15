5aef972e2697d900ccc70ab11d250e14e89f3656
David Wolff <davidwolff2020@u.northwestern.edu>
2017-07-15 06:22:17 -0700

    XIED THAT DAMN REVIEW

--- routes/routes.js
+++ routes/routes.js
@@ -65,205 +65,192 @@ router.get('/product/:id', function(req, res) {
 router.post('/product/:id',function(req,res){
   var review = req.body.reviewtext;
   var id = req.params.id;
-  var thereview = new Review({
+
+  new Review({
     owner: req.user._id,
     content: review,
     rating: 3
+  }).save(function(err, thereview) {
+    Product.findById(id)
+    .exec(
+      function(err,doc){
+
+        var length = doc.reviews.length+1;
+        console.log('length: ' + length)
+        if(length-1 !== 0 ){
+          var summedratings = 0;
+          for(var i = 0; i < doc.reviews.length; i++) {
+            summedratings += doc.reviews[i].rating
+          }
+          console.log(summedratings);
+          summedratings += thereview.rating;
+        }else{
+          var summedratings = thereview.rating;
+        }
+        console.log('summedratings: ' + summedratings)
+        var avg = summedratings/length;
+        console.log('avg: ' + avg);
+        var allreviews=[...doc.reviews,thereview];
+        console.log("allreviews: " + allreviews);
+        Product.findByIdAndUpdate(id, {
+          $set: {
+            productrating: avg,
+            reviews: allreviews
+           }
+         }, function(err, theproduct){
+           console.log("theproduct: " + theproduct)
+           if(err) {
+             console.log(err)
+           } else {
+             res.redirect("/marketplace")
+           }
+         })
+      })
   })
-  Product.findById(id)
-  .exec(
-    function(err,doc){
-      var length = doc.reviews.length+1;
-      function getSum(total, num) {
-        return total.rating + num.rating;
-        }
-      if(doc.reviews.length !== 0 ){
-      var summedratings=doc.reviews.reduce(getSum)+thereview.rating;
-    }else{
-      var summedratings = thereview.rating;
-    }
+  })
 
-      var avg = summedratings/length;
-      var allreviews=[...doc.reviews,thereview];
-      Product.findByIdAndUpdate(id,{productrating:avg, reviews:allreviews})
-      .exec(
-        function(err,thisproduct){
-          Product.find({owner:thisproduct.owner})
-          .exec(
-            function(err,allproductsfromuser){
-              function getSum(total, num) {
-                return total.productrating + num.productrating;
-                }
-              if(allproductsfromuser.length!==0){
-                var summedproductratings = allproductsfromuser.reduce(getSum)+avg;
-              }else{
-                var summedproductratings = avg;
-              }
 
-              var length = allproductsfromuser.length+1;
-              var avguserrating = summedproductratings/length;
-              User.findByIdAndUpdate(thisproduct.owner, {sellerrating:avguserrating})
-              .exec(
-                function(err,whatever){
-                  thereview.save(function(err){
-                    if(err){
-                    console.log('WHY THE FUCK WOULD YOU ERROR NOW?')
-                  }else{
-                    var redirecturl = '/product/'+id;
-                    res.redirect(redirecturl)
-                  }
-                  })
-                }
-              )
-            }
-          )
-        }
-      )
+
+  router.post('/newProduct', function(req, res) {
+    var img = req.file.mimetype.split('image/');
+    var path = req.file.path.split('public')[1]+'.'+img[1]
+    console.log(path)
+    var newProduct = new Product({
+      name: req.body.name,
+      pdf: path,
+      owner: req.user._id,
+      price: req.body.price,
+      course: req.body.course,
+      subject: req.body.subject,
+      school: req.user.school,
+      description: req.body.description
+    })
+    newProduct.save(function(err) {
+      if (err) {
+        console.log("product not saved");
+      }
+      else {
+        console.log('Product saved!');
+        fs.rename(req.file.path, req.file.path+'.'+img[1], function(err) {
+          if (err) {
+            console.log('Failed to update path to correct file type.');
+          }
+        })
+        Product.findOne({name: req.body.name, description: req.body.description}, function(err, product) {
+          if (err) {
+            console.log('error in finding saved product');
+          }
+          else {
+            res.redirect('/product/'+product._id)
+          }
+        })
+      }
     })
   })
 
 
 
-router.post('/newProduct', function(req, res) {
-  var img = req.file.mimetype.split('image/');
-  var path = req.file.path.split('public')[1]+'.'+img[1]
-  console.log(path)
-  var newProduct = new Product({
-    name: req.body.name,
-    pdf: path,
-    owner: req.user._id,
-    price: req.body.price,
-    course: req.body.course,
-    subject: req.body.subject,
-    school: req.user.school,
-    description: req.body.description
-  })
-  newProduct.save(function(err) {
-    if (err) {
-      console.log("product not saved");
+
+  ///////////////////////////// END OF PUBLIC ROUTES /////////////////////////////
+
+  router.use(function(req, res, next){
+    if (!req.user) {
+      res.redirect('/');
+    } else {
+      return next();
     }
-    else {
-      console.log('Product saved!');
-      fs.rename(req.file.path, req.file.path+'.'+img[1], function(err) {
-        if (err) {
-          console.log('Failed to update path to correct file type.');
-        }
-      })
-      Product.findOne({name: req.body.name, description: req.body.description}, function(err, product) {
-        if (err) {
-          console.log('error in finding saved product');
-        }
-        else {
-          res.redirect('/product/'+product._id)
-        }
-      })
-    }
-  })
-})
-
-
-
-
-///////////////////////////// END OF PUBLIC ROUTES /////////////////////////////
-
-router.use(function(req, res, next){
-  if (!req.user) {
-    res.redirect('/');
-  } else {
-    return next();
-  }
-});
-
-
-//////////////////////////////// PRIVATE ROUTES ////////////////////////////////
-// Only logged in users can see these routes
-
-router.get('/marketplace', function(req, res) {
-  console.log(req.user.school)
-  console.log(req.user.major)
-  Product.find({school: req.user.school,subject: req.user.major})
-  .populate('school')
-  .populate('owner')
-  .exec(
-    function(err,doc){
-      console.log(doc);
-      res.render('marketplace', {
-        product:doc,
-        user: req.user
-      })
-    })
   });
-  router.post('/marketplace', function(req,res){
-    var school = req.body.school_select;
-    if(school === 'Northwestern U'){
-      school='59695fa2d1770b298c31c3bb'
-    }else if(school === 'Hong Kong U'){
-      school='59695fa2d1770b298c31c3bc'
-    }else if(school === 'UC Berkeley'){
-      school='59695fa2d1770b298c31c3ba'
-    }else if(school === 'UC San Diego'){
-      school='59695fa2d1770b298c31c3b9'
-    }else{
-      school='all'
-    }
-    var find ={};
-    find['subject']=req.body.major_select;
-    if('all'!==school){
-      find.school=school;
-    }
-    var sort = {}
-    if(req.body.price_select = 'Rating'){
-      Product.find(find)
-      .populate('school')
-      .populate('owner')
-      .sort(sort)
-      .exec(
-        function(err,doc){
-          doc=doc.sort(function(a,b){
-            a.owner.sellerrating-b.owner.sellerrating
-          })
-          res.render('marketplace',{
-            product:doc,
-            user: req.user
-          })
-        }
-      )
-    }else if(req.body.price_select = 'Low to high'){
-      sort = {
-        price: -1
+
+
+  //////////////////////////////// PRIVATE ROUTES ////////////////////////////////
+  // Only logged in users can see these routes
+
+  router.get('/marketplace', function(req, res) {
+    console.log(req.user.school)
+    console.log(req.user.major)
+    Product.find({school: req.user.school,subject: req.user.major})
+    .populate('school')
+    .populate('owner')
+    .exec(
+      function(err,doc){
+        console.log(doc);
+        res.render('marketplace', {
+          product:doc,
+          user: req.user
+        })
+      })
+    });
+    router.post('/marketplace', function(req,res){
+      var school = req.body.school_select;
+      if(school === 'Northwestern U'){
+        school='59695fa2d1770b298c31c3bb'
+      }else if(school === 'Hong Kong U'){
+        school='59695fa2d1770b298c31c3bc'
+      }else if(school === 'UC Berkeley'){
+        school='59695fa2d1770b298c31c3ba'
+      }else if(school === 'UC San Diego'){
+        school='59695fa2d1770b298c31c3b9'
+      }else{
+        school='all'
       }
-      Product.find(find)
-      .populate('school')
-      .populate('owner')
-      .sort(sort)
-      .exec(
-        function(err,doc){
-          res.render('marketplace',{
-            product:doc,
-            user: req.user
-          })
-        }
-      )
-    }else{
-      sort = {
-        price: 1
+      var find ={};
+      find['subject']=req.body.major_select;
+      if('all'!==school){
+        find.school=school;
       }
-      Product.find(find)
-      .populate('school')
-      .populate('owner')
-      .sort(sort)
-      .exec(
-        function(err,doc){
-          res.render('marketplace',{
-            product:doc,
-            user: req.user
-          })
+      var sort = {}
+      if(req.body.price_select = 'Rating'){
+        Product.find(find)
+        .populate('school')
+        .populate('owner')
+        .sort(sort)
+        .exec(
+          function(err,doc){
+            doc=doc.sort(function(a,b){
+              a.owner.sellerrating-b.owner.sellerrating
+            })
+            res.render('marketplace',{
+              product:doc,
+              user: req.user
+            })
+          }
+        )
+      }else if(req.body.price_select = 'Low to high'){
+        sort = {
+          price: -1
         }
-      )
-    }
-  })
+        Product.find(find)
+        .populate('school')
+        .populate('owner')
+        .sort(sort)
+        .exec(
+          function(err,doc){
+            res.render('marketplace',{
+              product:doc,
+              user: req.user
+            })
+          }
+        )
+      }else{
+        sort = {
+          price: 1
+        }
+        Product.find(find)
+        .populate('school')
+        .populate('owner')
+        .sort(sort)
+        .exec(
+          function(err,doc){
+            res.render('marketplace',{
+              product:doc,
+              user: req.user
+            })
+          }
+        )
+      }
+    })
 
 
-  ///////////////////////////// END OF PRIVATE ROUTES /////////////////////////////
+    ///////////////////////////// END OF PRIVATE ROUTES /////////////////////////////
 
-  module.exports = router;
+    module.exports = router;
