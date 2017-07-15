eb478700b164da91b2d17e5e7e55d57173e37fe7
Johnathan Zhou <johnathanz@berkeley.edu>
2017-07-15 07:15:28 -0700

    hello

--- routes/routes.js
+++ routes/routes.js
@@ -56,6 +56,7 @@ router.get('/product/:id', function(req, res) {
       console.log(doc);
       res.render('singleproduct',{
         product: doc,
+        pkey: process.env.STRIPE_PKEY
       })
       //res.json(doc)
     }
