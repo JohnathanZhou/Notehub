e21b7741d395edb7eb45f20842e250d0d9a22bc0
David Wolff <davidwolff2020@u.northwestern.edu>
2017-07-14 17:18:36 -0700

    marketplace files

--- routes/routes.js
+++ routes/routes.js
@@ -18,7 +18,7 @@ router.get('/login', function(req, res) {
 })
 
 router.get('/home', function(req, res) {
-  res.render('home')
+  res.render('marketplace')
 })
 
 router.post('/',function(req,res){
